ce031e12c511fa4221efee57efd2f91a19e8bcc4
Alma <a.q.frankenstein@gmail.com>
2020-10-30 15:50:55 -0700

    add titleize method to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,16 @@
 class Book < ApplicationRecord
+  before_save(:titleize_book)
+
   validates :title, :author, :price, presence: true
+
   scope :search, -> (title, author) { where("title ilike ? AND author ilike ?", "%#{title}%", "%#{author}%") }
   scope :bargain, -> { order(price: :asc).limit(3) }
+
+  private
+  def titleize_book
+    self.name = self.name.titleize
+  end
 end
+
+
+
