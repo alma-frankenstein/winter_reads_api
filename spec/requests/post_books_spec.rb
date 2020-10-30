2d455c9df8ef8ffc3675b3ab8fa9a77495d1e05c
Alma <a.q.frankenstein@gmail.com>
2020-10-30 11:12:46 -0700

    pass rspec for successful post request

---
+++ spec/requests/post_books_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "add a book through post to /books route", :type => :request do
+
+  before do
+    post '/books', params: { :title => 'The Seas', :author => 'Sam Hunt', :price =>6 }
+  end
+
+  it 'returns the title of the book' do
+    expect(JSON.parse(response.body)['title']).to eq('The Seas')
+  end
+
+  it 'returns created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
