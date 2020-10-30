91d41835c40e4da354c575e0767c89a909989d1b
Alma <a.q.frankenstein@gmail.com>
2020-10-30 14:15:30 -0700

    missing params returns correct fail staus

--- spec/requests/post_book_fail_spec.rb
+++ spec/requests/post_book_fail_spec.rb
@@ -10,7 +10,7 @@ describe "add a book through post to /books route", :type => :request do
     expect(JSON.parse(response.body)['message']).to eq("Validation failed: Author can't be blank")
   end
 
-  # it 'returns created status' do
-  #   expect(response).to have_http_status(:unprocessable_entity)
-  # end
+  it 'returns created status' do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 end
