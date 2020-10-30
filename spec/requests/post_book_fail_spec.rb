ec0586245f78f5bc7e9a7969b7fa2a474d02869f
Alma <a.q.frankenstein@gmail.com>
2020-10-30 14:14:45 -0700

    missing params fails well

---
+++ spec/requests/post_book_fail_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "add a book through post to /books route", :type => :request do
+
+  before do
+    post '/books', params: { :title => 'The Seas', :price =>6 }
+  end
+
+  it 'returns an error about missing author param' do
+    expect(JSON.parse(response.body)['message']).to eq("Validation failed: Author can't be blank")
+  end
+
+  # it 'returns created status' do
+  #   expect(response).to have_http_status(:unprocessable_entity)
+  # end
+end
