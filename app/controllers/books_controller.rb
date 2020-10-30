c79521dba14db552999fefd191aee8629b21ed29
Alma <a.q.frankenstein@gmail.com>
2020-10-30 08:41:49 -0700

    add actions to controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    json_response(@books)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    json_response(@book)
+  end
+
+  def create
+    @book = Book.create!(book_params)
+    json_response(@book, :created)
+  end
+
+  # def random
+  #   @book = Book.all.sample
+  #   json_response(@book)
+  # end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update!(book_params)
+      render status: 200, json: {
+        message: "book had been updated"
+      }
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      render status: 200, json: {
+        message: "book destroyed!!!"
+      }
+    end
+  end
+
+
+  private
+  def book_params
+    params.permit(:author, :title, :price)
+  end
+
+end
